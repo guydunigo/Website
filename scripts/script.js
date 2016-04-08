03d94c380dd4a92a8867064dc89a0b81d6641fb4
Guillaume GONI <guydunigo@gmail.com>
2016-04-08 11:57:41 +0200

    Added some text and a fiew other things

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,33 @@
+var slideTitleLeft = function() {
+    $("#webTitle").animate({
+        "margin-left": "-=10px",
+        "margin-right": "+=10px"
+    }, "fast")
+}
+var slideTitleRight = function() {
+    $("#webTitle").animate({
+        "margin-left": "+=15px",
+        "margin-right": "-=15px"
+    }, "fast")
+    $("#webTitle").animate({
+        "margin-left": "-=5px",
+        "margin-right": "+=5px"
+    }, "fast")
+}
+
 var main = function() {
-    // Bounce effect on website name
-    var headerBarElement = $("div.bar h1 a")
-    $("div.bar h1 a").hover(function() {
-        $(this).animate({
-            "margin-left": "-=10px",
-            "margin-right": "+=10px"
-        }, "fast")
-    }, function() {
-        $(this).animate({
-            "margin-left": "+=15px",
-            "margin-right": "-=15px"
-        }, "fast")
-        $(this).animate({
-            "margin-left": "-=5px",
-            "margin-right": "+=5px"
-        }, "fast")
-    });
+    // Bounce effect on website name when the page loads and when hovered :
+    slideTitleLeft();
+    slideTitleRight();
+    $("#webTitle").hover(slideTitleLeft, slideTitleRight);
+    // Darken sections titles when hovered :
     $("div.bar li a").hover(function() {
         $(this).animate({ color: "#000" }, "fast");
     }, function() {
         $(this).animate({ color: "#ccc" }, "fast");
     });
 
-    // Scroll to element :
+    // Scroll to elements :
     $("a#aboutButton").click(function() {
         $('html, body').animate({
             'scrollTop': $("div#aboutMe").offset().top
