65d47e4af44abb83f852ece8735bdf70501d7ec1
Guillaume GONI <gdn3@live.fr>
2016-06-08 12:30:54 +0200

    Mise à jour des libs et suppression de trucs trucs inutiles

---
+++ scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../pages/script.js" />
+/// <reference path="jquery-2.2.4.js" />
+/// <reference path="jquery-ui.min.js" />
+/// <reference path="script.js" />
